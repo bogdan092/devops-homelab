1a050c9e759105bc98d2dc8a9dc9ca133e9e6442
bogdan <cojocariu.bogdan92@yahoo.com>
2026-03-05 22:33:20 +0200

    fix: scale to 20GB disks, fix cloud-init SSH keys, and add 2nd node

--- 01-terraform/main.tf
+++ 01-terraform/main.tf
@@ -1,55 +1,74 @@
 terraform {
   required_providers {
     libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "0.7.6"
+      source = "dmacvicar/libvirt"
     }
   }
 }
 
-# Configure the libvirt provider to connect to your local machine
+# Connect to the system-wide KVM hypervisor
 provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# 1. Fetch the official Ubuntu 22.04 Cloud Image from the internet
-resource "libvirt_volume" "ubuntu_image" {
-  name   = "ubuntu-jammy.qcow2"
-  pool   = "default" # This is KVM's default storage area
+# ==========================================
+# Shared Resources
+# ==========================================
+
+# 1. Cloud-Init ISO for Node 1
+resource "libvirt_cloudinit_disk" "commoninit_01" {
+  name      = "commoninit-01.iso"
+  pool      = "default"
+  user_data = templatefile("${path.module}/cloud_init.cfg", {
+    ssh_key = trimspace(file(pathexpand("~/.ssh/id_ed25519.pub")))
+  })
+}
+
+# 1.1. Cloud-Init ISO for Node 2
+resource "libvirt_cloudinit_disk" "commoninit_02" {
+  name      = "commoninit-02.iso"
+  pool      = "default"
+  user_data = templatefile("${path.module}/cloud_init.cfg", {
+    ssh_key = trimspace(file(pathexpand("~/.ssh/id_ed25519.pub")))
+  })
+}
+# 2. Base Ubuntu Image (Downloaded once, acts as a read-only template)
+resource "libvirt_volume" "ubuntu_base" {
+  name   = "ubuntu-base.img"
+  pool   = "default"
   source = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
   format = "qcow2"
 }
 
-# 2. Create the Cloud-Init disk using our config file
-resource "libvirt_cloudinit_disk" "commoninit" {
-  name      = "commoninit.iso"
-  pool      = "default"
-  user_data = templatefile("${path.module}/cloud_init.cfg", {
-    # This reads your public key from your Ubuntu machine and passes it to the config
-    ssh_key = file("~/.ssh/id_ed25519.pub")
-  })
+# ==========================================
+# Node 01 Configuration
+# ==========================================
+
+# 3. Node 1 Hard Drive (Cloned from base, expanded to 20GB)
+resource "libvirt_volume" "ubuntu_image" {
+  name           = "ubuntu-jammy.qcow2"
+  pool           = "default"
+  base_volume_id = libvirt_volume.ubuntu_base.id
+  size           = 21474836480 # 20GB in bytes
 }
 
-# 3. Define the Virtual Machine itself
+# 4. Node 1 Virtual Machine
 resource "libvirt_domain" "docker_node" {
   name   = "docker-node-01"
-  memory = "2048" # 2GB RAM
-  vcpu   = 2      # 2 CPU Cores
+  memory = "2048"
+  vcpu   = 2
 
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
+  cloudinit = libvirt_cloudinit_disk.commoninit_01.id
 
-  # Connect to KVM's default virtual network
   network_interface {
     network_name   = "default"
-    wait_for_lease = true # Wait until the VM gets an IP address
+    wait_for_lease = true
   }
 
-  # Attach the Ubuntu hard drive we downloaded
   disk {
     volume_id = libvirt_volume.ubuntu_image.id
   }
 
-  # Setup a virtual console
   console {
     type        = "pty"
     target_port = "0"
@@ -57,32 +76,25 @@ resource "libvirt_domain" "docker_node" {
   }
 }
 
-# 4. Output the IP Address so we know how to connect to it!
-output "vm_ip" {
-  value       = try(libvirt_domain.docker_node.network_interface[0].addresses[0], "IP not assigned yet or VM is off")
-  description = "The IP address of the first Docker Node"
-}
-
 # ==========================================
-# node-02 configuration
+# Node 02 Configuration
 # ==========================================
 
-# 1. Create a separate hard drive for the second VM
+# 5. Node 2 Hard Drive (Cloned from base, expanded to 20GB)
 resource "libvirt_volume" "ubuntu_image_02" {
-  name   = "ubuntu-jammy-02.qcow2"
-  pool   = "default"
-  source = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-  format = "qcow2"
+  name           = "ubuntu-jammy-02.qcow2"
+  pool           = "default"
+  base_volume_id = libvirt_volume.ubuntu_base.id
+  size           = 21474836480 # 20GB in bytes
 }
 
-# 2. Define the second Virtual Machine
+# 6. Node 2 Virtual Machine
 resource "libvirt_domain" "docker_node_02" {
   name   = "docker-node-02"
   memory = "2048"
   vcpu   = 2
 
-  # We reuse the exact same Cloud-Init ISO from the first node!
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
+  cloudinit = libvirt_cloudinit_disk.commoninit_02.id
 
   network_interface {
     network_name   = "default"
@@ -100,7 +112,15 @@ resource "libvirt_domain" "docker_node_02" {
   }
 }
 
-# 3. Output the IP Address for node 2
+# ==========================================
+# Outputs
+# ==========================================
+
+output "vm_ip" {
+  value       = try(libvirt_domain.docker_node.network_interface[0].addresses[0], "IP not assigned yet or VM is off")
+  description = "The IP address of the first Docker Node"
+}
+
 output "vm_ip_02" {
   value       = try(libvirt_domain.docker_node_02.network_interface[0].addresses[0], "IP not assigned yet or VM is off")
   description = "The IP address of Docker Node 02"
